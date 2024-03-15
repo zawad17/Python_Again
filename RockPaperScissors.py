83221576cbe5b314f4d6b2c99db858135765dff8
Zawad Rahman <138771190+zawad17@users.noreply.github.com>
2024-03-15 14:58:25 +0600

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,19 +1,19 @@
 import random
 
-List = ["Rock", "Paper", "Scissors"]
+List = ["rock", "paper", "scissors"]
 
 while True:
     computer_count = 0
     your_count = 0
 
-    you_choice = int(input('''
+    your_choice = int(input('''
                 GAME START....
                 1.Yes
                 2.No | Exit
                 '''))
 
-    if you_choice == 1:
-        for _ in range(1, 6):
+    # if you_choice == 1:
+    for a in range(0, 5):
             you_choice = int(input('''
                 1. Rock
                 2. Paper
@@ -26,24 +26,24 @@ while True:
                 print("Draw")
                 your_count += 1
                 computer_count += 1
-            elif (you_choice == 1 and computer_choice == 2) or (you_choice == 2 and computer_choice == 0) or (you_choice == 0 and computer_choice == 1):
+            elif (you_choice == 1 and computer_choice == "scissors") or (you_choice == 2 and computer_choice == "rock") or (you_choice == 3 and computer_choice == "paper"):
                 print("You win!")
                 your_count += 1
             else:
                 print("You lose.")
                 computer_count += 1
 
-        print("Final Scores:")
-        print("Your Score:", your_count)
-        print("Computer Score:", computer_count)
+    print("Final Scores:")
+    print("Your Score:", your_count)
+    print("Computer Score:", computer_count)
 
-    else:
-        print("Invalid number")
-        break
+    # else:
+    #     print("Invalid number")
+    #     break
 
 
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != 'y':
-        break
+# play_again = input("Play again? (y/n): ")
+#            if play_again.lower() != 'y':
+#                  break
 
-print("Thanks for playing!")
+
